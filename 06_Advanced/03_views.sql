a5ad492fc41f93e855ee42d5a2d5ffc789f8a0ee
EliDev <eliasss.mb@gmail.com>
2025-01-06 23:26:13 -0300

    add mysql

--- 06_Advanced/03_views.sql
+++ 06_Advanced/03_views.sql
@@ -13,7 +13,7 @@ VIEWS
 Lección 18.3: https://youtu.be/OuJerKzV5T0?t=19663
 */
 
--- Crea unaa vista llamada "v_adult_users" con los nombres y edades de usuarios de la table "users"
+-- Crea una vista llamada "v_adult_users" con los nombres y edades de usuarios de la table "users"
 -- que tienen una edad igual o mayor a 18 años.
 CREATE VIEW v_adult_users AS
 SELECT name, age
